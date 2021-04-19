fb27f6b85caa2f516fc8522235ea66b22c21fd21
stuypend <andre@stuypend.com>
2021-04-19 14:29:25 -0400

    started

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-// write JS code here
+let board = []
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    initializeBoard()
+    console.log(board)
+
+})
+
+function initializeBoard()
+{
+    let table = document.querySelector(".board")
+
+    for (let i = 0; i < 10; i++)
+    {
+        board.push([])
+        let newRow = document.createElement("tr")
+        newRow.dataset.row = i
+        for (let j = 0; j < 10; j++)
+        {
+            board[i][j] = "gray"
+            let cell = document.createElement("td")
+            cell.dataset.row = i
+            cell.dataset.col = j
+            newRow.append(cell)
+        }
+        table.append(newRow)
+    }
+}
+
+
+
+
 
