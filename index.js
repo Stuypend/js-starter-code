be99f2ce6979dc9e25895259ce1013660e5741ad
stuypend <andre@stuypend.com>
2021-04-19 17:33:11 -0400

    movements and checks

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let table = initializeBoard()
     console.log(board)
 
-    addNextPiece(longPiece)
+    addNewPiece(longPiece)
 
 })
 
@@ -37,25 +37,23 @@ function initializeBoard()
     return table
 }
 
-function addNextPiece(pieceType) {
+function addNewPiece()
+{
+    // generate a piece
+    updateBoard(longPiece)
 
-    pieceType.coordinates.forEach(coord => { // x and y coordindates
-        // debugger
-        board[coord[0]] [coord[1]] = pieceType.color
-        updateCell(coord[0], coord[1])
+    // call updateBoard
+}
+
+function updateBoard(pieceType) {
+
+    pieceType.coordinates.forEach(coord => { // x and y coordindate
+        updateCell(coord[0], coord[1], pieceType.color)
     });
 
 }
 
 let longPiece = { coordinates: [ [0,0], [0,1], [0,2], [0,3] ], color: "blue"}
-
-function updateCell(x, y) {
-    let row = document.querySelector(`[data-row="${x}"]`)
-
-    let cell = row.querySelector(`[data-col="${y}"]`)
-    // debugger
-    cell.style.backgroundColor = board[x][y]
-}
 
 function timeStep(pieceType) {
     pieceType.coordinates.forEach(coord => { // x and y coordindates
@@ -67,4 +65,122 @@ function timeStep(pieceType) {
     addNextPiece(pieceType)
 
 }
+
+function isInBounds(piece, move)
+{
+    let coords =  piece.coordinates
+
+   /* switch (move) {
+        case "right":
+            coords.forEach(coord => {
+                if (coord[1] + 1 > board[0].length)
+                    return false;
+
+            })
+            break;*/
+
+        if(move == "left") {
+            debugger
+            coords.forEach(coord => {
+                if (coord[1] - 1 < 0) {
+                    return false;
+                }
+
+            })
+        }
+    /*
+            break;
+        case "down":
+            coords.forEach(coord => {
+                if (coord[0] + 1 > board[0][0].length)
+                    return false;
+
+            })
+    }*/
+
+    return true
+
+
+}
+
+function isOpen(piece, move)
+{
+    let coords =  piece.coordinates
+
+    switch (move) {
+        case "right":
+            coords.forEach(coord => {
+                if (board[coord[0]][coord[1]+1] != "gray")
+                    return false
+
+            })
+            break;
+        case "left":
+            coords.forEach(coord => {
+                if (board[coord[0]][coord[1]-1] != "gray")
+                    return false
+
+            })
+            break;
+        case "down":
+            coords.forEach(coord => {
+                if (board[coord[0]+1][coord[1]] != "gray")
+                    return false
+
+            })
+    }
+    return true
+
+
+}
+
+function moveRight(piece)
+{
+    if (isInBounds(piece, "right") && isOpen(piece, "right"))
+    {
+        piece.coordinates.forEach(coord => { // x and y coordindates
+            updateCell(coord[0], coord[1]) //changing the table cell to gray
+            coord[1]++
+        });
+        updateBoard(piece)
+    }
+
+}
+
+function moveLeft(piece)
+{
+    if (isInBounds(piece, "left") && isOpen(piece, "left"))
+    {
+        piece.coordinates.forEach(coord => { // x and y coordindates
+            updateCell(coord[0], coord[1])
+            coord[1]--
+        });
+        updateBoard(piece)
+    }
+
+}
+
+function moveDown(piece)
+{
+    if (isInBounds(piece, "down") && isOpen(piece, "down"))
+    {
+        piece.coordinates.forEach(coord => { // x and y coordindates
+            updateCell(coord[0], coord[1])
+            coord[0]++
+        });
+        updateBoard(piece)
+    }
+
+}
+
+function updateCell(row, col, color="gray")
+{
+    board[row][col] = color
+
+    let tableRow = document.querySelector(`[data-row="${row}"]`)
+    let tableCell = tableRow.querySelector(`[data-col="${col}"]`)
+
+    tableCell.style.backgroundColor = color
+}
+
 
